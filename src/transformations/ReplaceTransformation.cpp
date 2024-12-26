9de755f097c9af685cb36f00bb4e19d3f06822ec
Andrey Todorov <andrey.etodorov@gmail.com>
2024-12-26 11:08:56 +0200

    Changed behaiviour and added additional tests to replace transformation

--- src/transformations/ReplaceTransformation.cpp
+++ src/transformations/ReplaceTransformation.cpp
@@ -2,7 +2,6 @@
 
 ReplaceTransformation::ReplaceTransformation(const std::string& to_replace, const std::string& replacement) : to_replace(to_replace), replacement(replacement) {
     if (to_replace.empty()) throw std::invalid_argument("No argument for replacement given");
-    if (replacement.empty()) throw std::invalid_argument("No replacement given");
 }
 
 std::string ReplaceTransformation::transform(const std::string& text) const {
--- test/transformations-tests/test-replace.cpp
+++ test/transformations-tests/test-replace.cpp
@@ -7,13 +7,28 @@ TEST_CASE("Single occurance replacement", "[ReplaceTransformation]") {
 }
 
 TEST_CASE("Multiple occurances replacement", "[ReplaceTransformation]") {
-    ReplaceTransformation replace("nibba", "friend");
-    REQUIRE(replace.transform("Hey, nibba, what up, my nibba") == "Hey, friend, what up, my friend");
+    ReplaceTransformation replace("homie", "friend");
+    REQUIRE(replace.transform("Hey, homie, what up, my homie") == "Hey, friend, what up, my friend");
 }
 
 TEST_CASE("Word to replace is contained within a word", "[ReplaceTransformation]") {
-    ReplaceTransformation replace("bb", "xx");
-    REQUIRE(replace.transform("Hey, nibba, what up, my nibba") == "Hey, nixxa, what up, my nixxa");
+    ReplaceTransformation replace("i", "o");
+    REQUIRE(replace.transform("Hey, homie, what up, my homie") == "Hey, homoe, what up, my homoe");
+}
+
+TEST_CASE("Replace substring within a word, replacement is longer than original", "[ReplaceTransformation]") {
+    ReplaceTransformation replace("omi", "fasollasido");
+    REQUIRE(replace.transform("Hey, homie, what up, my homie") == "Hey, hfasollasidoe, what up, my hfasollasidoe");
+}
+
+TEST_CASE("Replace substring within a word, replacement is shorter than original", "[ReplaceTransformation]") {
+    ReplaceTransformation replace("omi", "a");
+    REQUIRE(replace.transform("Hey, homie, what up, my homie") == "Hey, hae, what up, my hae");
+}
+
+TEST_CASE("Replace with empty string", "[ReplaceTransformation]") {
+    ReplaceTransformation replace("homie", "");
+    REQUIRE(replace.transform("Hey, homie, what up, my homie") == "Hey, , what up, my ");
 }
 
 TEST_CASE("Word to replace not found, throws", "[ReplaceTransformation]") {
@@ -28,8 +43,4 @@ TEST_CASE("Empty text, throws", "[ReplaceTransformation]") {
 
 TEST_CASE("Nothing to replace, throws", "[ReplaceTransformation]") {
     REQUIRE_THROWS_WITH(ReplaceTransformation("", "fun"), "No argument for replacement given");
-}
-
-TEST_CASE("Replacement is empty, throws", "[ReplaceTransformation]") {
-    REQUIRE_THROWS_WITH(ReplaceTransformation("fun", ""), "No replacement given");
 }
